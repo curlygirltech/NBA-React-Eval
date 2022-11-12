c48632c6dfde078b8edcb22fb6927ca46e7fee75
jcameron10 <jovonnec@gmail.com>
2022-11-11 23:22:04 -0500

    teams and players API endpoint

---
+++ nba-react-eval/pages/api/teams.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {teams} from '../data'
+
+type Data = {
+
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json( teams )
+}
