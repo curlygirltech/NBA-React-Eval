108c6e9448a1d719b2a4e8a66c6909c07907f482
jcameron10 <jovonnec@gmail.com>
2022-11-15 12:01:59 -0500

    playercard component

---
+++ nba-react-eval/pages/components/PlayerCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export default function PlayerCard() {
+
+  
+  return (
+    <>
+      <div>
+      {/* {playerJsonData.map((data: any) => (
+              <div key={data.pid} className="headshot">
+                {data.headshot}
+                {data.fn}, {data.ln} */}
+       <h3>player data here</h3>
+      
+    </div>
+    </>
+  )
+}
--- nba-react-eval/pages/index.tsx
+++ nba-react-eval/pages/index.tsx
@@ -1,13 +1,44 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+import LoadingScreen from "./components/LoadingScreen";
+import PlayerCard from "./components/PlayerCard";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [teamJsonData, setTeamJsonData] = useState<any>(null);
+  const [playerJsonData, setPlayerJsonData] = useState<any>([]);
+
+  useEffect(() => {
+    //fetch teams
+    async function fetchJsonData(): Promise<any> {
+      const response = await fetch(`/api/teams`);
+      const teamData = await response.json();
+      setTeamJsonData(teamData);
+      console.log("LOOK HERE", { teamData });
+
+      //fetch players
+      const playerresponse = await fetch(`/api/players`);
+      const playerData = await playerresponse.json();
+      setPlayerJsonData(playerData);
+      console.log("Player data HERE", { playerData });
+
+      setIsLoading(false);
+    }
+    fetchJsonData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      {!isLoading ? (
+        <>
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <div>
+            <PlayerCard player={firstName: {data.fn},} />
+          </div>
+        </>
+      ) : (
+        <LoadingScreen />
+      )}
     </div>
-  )
+  );
 }
